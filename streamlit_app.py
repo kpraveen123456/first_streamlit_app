5b4fa650599abbab137744533896cdebb193865f
kpraveen123456 <136667749+kpraveen123456@users.noreply.github.com>
2023-06-15 10:32:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Health Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluberry Oatmeal ')
+streamlit.text('Kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
