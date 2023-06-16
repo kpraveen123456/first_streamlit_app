b93cec03653aad90b4bf092d8bc9c0ae8f98b3c4
kpraveen123456 <136667749+kpraveen123456@users.noreply.github.com>
2023-06-16 17:25:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#create the repeatable code block(called a function)
+def get_fruityvice_data(this_fruit_choice);
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json()) 
+     return fruityvice_normalized
+  
 # New Section to display Fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+      streamlit.error("please select a fruit to get information.")
+else:
+    back_from_function=get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 streamlit.write('The user entered ', fruit_choice)
 
 #import requests
@@ -41,11 +53,17 @@ streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
 
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
+#snowflake-related functions
+def get_fruit_load_list():
+     with my_cnx.cursor() as my_cur:
+       my_cur.execute("select * from fruit_load_list")
+       return my_cur.fetchall()
+
+#Add a buttom to load the fruit
+if streamlit.buttom('Get Fruit Load List'):
+  my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows=get_fruit_load_list()
 streamlit.dataframe(my_data_rows)
 
 #Allow the end user to add a fruit to the list
@@ -54,4 +72,14 @@ streamlit.write('Thanks for adding ',add_my_fruit)
 
 #This will not work correctly,but just go with it for now
 my_cur.execute("insert into fruit_load_list values('from streamlit')")
+
+
+
+
+
+
+
+
+
+
 
