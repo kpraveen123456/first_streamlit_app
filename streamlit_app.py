0f7c6e3708b925a769ec08da81b94d6f05c22245
kpraveen123456 <136667749+kpraveen123456@users.noreply.github.com>
2023-06-20 13:00:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,8 +50,10 @@ try:
 #streamlit.stop()
 
 #import snowflake.connector
+# New Section to display Fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
 
-streamlit.header("The fruit load list contains:")
+#streamlit.header("The fruit load list contains:")
 #snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
